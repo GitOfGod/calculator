99457453c41574d2700961c926ba679554d4d6fa
Favour <favouramike@gmail.com>
2024-06-27 13:19:49 +0100

    add event listeners to all buttons

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ let firstNumber;
 let secondNumber;
 let operator;
 
+function addEventListeners() {
+    //digit event listeners
+    let digits = document.querySelectorAll('.digit');
+    digits.forEach(digit => {
+        addEventListener('click', populateDisplay(digit));
+    })
+
+    //operator event listeners
+    let operators = document.querySelectorAll('.operator');
+    operators.forEach(operator => {
+        addEventListener('click', populateDisplay(operator));
+    })
+
+    //clear event listener
+    let clear = document.querySelector('.clear');
+    addEventListener('click', populateDisplay(operator));
+
+}
+
 function add(a, b) {
     return a + b;
 }

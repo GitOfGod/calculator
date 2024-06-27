4aa186d311c40b38a214c1e408feaa30a6e70ee8
Favour <favouramike@gmail.com>
2024-06-27 14:26:39 +0100

    Display content when corresponding button clicked
    - Attach event listeners to specific buttons to avoid all digits being
    displayed at once
    - Call event listener function once DOM is loaded

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 let firstNumber;
 let secondNumber;
 let operator;
+let display = document.querySelector('.display');
+
+function populateDisplay(content) {
+    display.textContent = content;
+}
 
 function addEventListeners() {
     //digit event listeners
     let digits = document.querySelectorAll('.digit');
     digits.forEach(digit => {
-        addEventListener('click', populateDisplay(digit));
+        digit.addEventListener('click', () => populateDisplay(digit.textContent));
     })
 
     //operator event listeners
     let operators = document.querySelectorAll('.operator');
     operators.forEach(operator => {
-        addEventListener('click', populateDisplay(operator));
+        operator.addEventListener('click', () => populateDisplay(operator.textContent));
     })
 
     //clear event listener
     let clear = document.querySelector('.clear');
-    addEventListener('click', populateDisplay(operator));
+    clear.addEventListener('click', () => populateDisplay(''));
 
 }
 
@@ -56,3 +61,6 @@ function operate(firstNumber, secondNumber, operator) {
     }
     return answer;
 }
+
+// Add event listeners when the DOM content is loaded
+document.addEventListener('DOMContentLoaded', addEventListeners);
